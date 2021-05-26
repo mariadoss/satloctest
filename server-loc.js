009e74a5e2321092055ef42becc3fa9482a6956b
mariadoss <pandianmd@gmail.com>
2021-05-25 22:23:46 -0500

    Update server-loc.js

--- server-loc.js
+++ server-loc.js
@@ -7,6 +7,10 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 app.get('/locationinfo', (req, resp) => {
   //res.send('Hello World');
 	console.log('Calling loctioninfo');
