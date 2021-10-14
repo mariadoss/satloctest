ad4b626800fc5ff2b8348de24f87e42245bc4aed
mariadoss <pandianmd@gmail.com>
2021-10-13 19:43:12 -0500

    mvi change

--- server-loc.js
+++ server-loc.js
@@ -38,6 +38,32 @@ app.get('/locationinfo', (req, resp) => {
         request.end();
 });
 
+app.get('/mvi', (req, resp) => {
+  //res.send('Hello World');
+	console.log('Calling mvi');
+        var options = {
+        host: 'c-04.private.us-east.link.satellite.cloud.ibm.com',
+	port: 33823,
+	path: '/api/dlapis/111d99ce-3bf0-4cfc-bea6-b9b2759a03c9?imageUrl=http://maximo-query-ui-git-lab-4-admin.dronelabs-ed9112eb83c761afd4c566b0882eaa3e-0000.us-south.containers.appdomain.cloud/Picture1.png',
+        }
+        var data = '';
+        var request = http.request(options, function (res) {
+
+                res.on('data', function (chunk) {
+                        data += chunk;
+                });
+                res.on('end', function () {
+                        console.log(data);
+                        resp.send(data);
+                });
+        });
+        request.on('error', function (e) {
+                console.log(e.message);            
+
+        });
+        //request.end();
+        request.end();
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
